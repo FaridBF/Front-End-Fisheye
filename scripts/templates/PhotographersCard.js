2387c7467bce73d6e6a33035587b8713b8af73bf
Farid <bouras.farid@outlook.fr>
2022-10-03 18:00:50 +0200

    feat: added template for the photographerCard

---
+++ scripts/templates/PhotographersCard.js
@@ -0,0 +1,27 @@
+// permet de retourner le template ou la vue
+// son rôle est d'afficher les informations
+class PhotographersCard {
+  constructor(photographer) {
+    this._photographer = photographer;
+  }
+  userCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    // img.setAttribute("src", picture);
+    // h2.textContent = name;
+    const h2 = document.createElement("h2");
+    article.appendChild(img);
+    article.appendChild(h2);
+
+    const userCardDOM = `
+    <img
+    alt="${this._photographer.name}"
+    src="${this._photographer.portrait}"
+    >
+    <h2>${this._photographer.name}<h2/>
+    `;
+
+    article.innerHTML = userCardDOM;
+    return article;
+  }
+}
