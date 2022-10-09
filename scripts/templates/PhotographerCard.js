ea29c0527d1b42b2f200fc4afbdf9790c74e5fba
Farid <bouras.farid@outlook.fr>
2022-10-09 18:13:54 +0200

    feat: added image in card article and manage accessibilité

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -7,19 +7,31 @@ class PhotographerCard {
   }
   userCardDOM() {
     const article = document.createElement("article");
-    const img = document.createElement("img");
-    // img.setAttribute("src", picture);
-    // h2.textContent = name;
-    const h2 = document.createElement("h2");
-    article.appendChild(img);
-    article.appendChild(h2);
+    // const img = document.createElement("img");
+    // const div = document.createElement("div");
+    // const h2 = document.createElement("h2");
+    // const p = document.createElement("p");
+    // const span = document.createElement("span");
 
+    // article.appendChild(img);
+    // article.appendChild(div);
+
+    // div.appendChild(h2);
+    // div.appendChild(p);
+    // div.appendChild(span);
+
+    // comment éviter l'aspect redondant ici
     const userCardDOM = `
-      <img
-        alt="${this._photographer.name}"
-        src="${this._photographer.portrait}"
+      <img 
+        class="img_profil"
+        alt=${this._photographer.name}
+        src=${this._photographer.portrait}
       >
-      <h2>${this._photographer.name}<h2/>
+        <h2 
+        > ${this._photographer.name}<h2/>
+        <p>${this._photographer.location}<p/>
+        <p>${this._photographer.tagline}<p/>
+        <span>${this._photographer.price}/jour<span/>
     `;
 
     article.innerHTML = userCardDOM;
